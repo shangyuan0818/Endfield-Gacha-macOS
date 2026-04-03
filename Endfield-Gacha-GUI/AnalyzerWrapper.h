2857c2f826a1443d520cca8b34276d3a9985a6f6
shangyuan0818 <61129284+shangyuan0818@users.noreply.github.com>
2026-04-03 00:29:10 -0400

    GUI

---
+++ Endfield-Gacha-GUI/AnalyzerWrapper.h
@@ -0,0 +1,22 @@
+//
+//  AnalyzerWrapper.h
+//  Endfield-Gacha
+//
+//  Created by Yin Shangyuan on 2026-04-02.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h> // 需要用到 NSImage
+
+// 把分析结果打包成一个对象传给 Swift
+@interface AnalysisResult : NSObject
+@property (nonatomic, strong) NSString *textOutput;
+@property (nonatomic, strong) NSImage *chartImage;
+@end
+
+@interface AnalyzerWrapper : NSObject
++ (AnalysisResult *)analyzeFile:(NSString *)filePath
+                          chars:(NSString *)chars
+                           pool:(NSString *)pool
+                           weps:(NSString *)weps;
+@end
---
+++ Endfield-Gacha-GUI/Endfield_GachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Endfield_GachaApp.swift
+//  Endfield-Gacha
+//
+//  Created by Yin Shangyuan on 2026-04-02.
+//
+
+import SwiftUI
+
+@main
+struct Endfield_GachaApp: App {
+    var body: some Scene {
+        WindowGroup("终末地抽卡记录分析与可视化") {
+            ContentView()
+        }
+    }
+}
